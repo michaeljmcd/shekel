faef4441c79de15c9bc7d734270bbe36bca8b35c
Michael McDermott <michael.mcdermott@jbhunt.com>
2018-09-19 10:33:00 -0500

    Reformatted file; setting up client.

--- main.go
+++ main.go
@@ -1,32 +1,34 @@
 package main
 
 import (
-        "os"
-        "fmt"
-        "github.com/urfave/cli"
-       )
+	"fmt"
+	"github.com/urfave/cli"
+	"golang.org/x/oauth2"
+	"os"
+)
 
 func main() {
-    app := configureCommandLineParsing()
-    app.Run(os.Args)
-
-    //fmt.Println("Hello")
+	app := configureCommandLineParsing()
+	app.Run(os.Args)
 }
 
 func configureCommandLineParsing() *cli.App {
-//askCommand := flag.NewFlagSet("oauth2", flag.ExitOnError)
-//    askCommand.String("stuff", "", "Stuff")
-    app := cli.NewApp()
-    app.Commands = []cli.Command{
-        {
-            Name: "oauth2",
-            Usage: "stuff",
-            Action: func(c *cli.Context) error {
-                fmt.Println("oauth stuff!")
-                return nil
-            },
-        },
-    }
+	app := cli.NewApp()
+	app.Name = "shekel"
+	app.Usage = "A command-line client for OAuth 2.0"
 
-    return app
+	app.Commands = []cli.Command{
+		{
+			Name:   "oauth2",
+			Usage:  "stuff",
+			Action: executeOAuth2Request,
+		},
+	}
+
+	return app
+}
+
+func executeOAuth2Request(c *cli.Context) error {
+	fmt.Println("oauth stuff!")
+	return nil
 }
